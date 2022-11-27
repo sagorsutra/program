75153e4928efe6e078d6aa71ea51982b9481d445
Sagor <sagorsutra16@gmail.com>
2022-11-27 20:27:28 +0600

    finding element with it's index

--- CollectionProblem_W3resource/src/Arraylist/insrt_1st.java
+++ CollectionProblem_W3resource/src/Arraylist/insrt_1st.java
@@ -5,6 +5,36 @@ import java.util.ArrayList;
 public class insrt_1st {
     public static void main(String[] args) {
 
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Red");
+        arrayList.add("Black");
+        arrayList.add("yellow");
+        arrayList.add("Green");
+        arrayList.add("Purple");
+
+
+        if (arrayList.contains("Green"))
+        {
+            System.out.println("Yes");
+            System.out.println("The index is "+ arrayList.indexOf("Green"));
+
+        }
+        else
+            System.out.println("No");
+
+
+
+
+
+
+    }
+}
+
+
+
+
+        /*
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Red");
         arrayList.add("Green");
@@ -18,7 +48,36 @@ public class insrt_1st {
         System.out.println("after adding\n");
         arrayList.add(0,"Gray");
         System.out.println(arrayList);
+         */
 
 
-    }
-}
+
+//updating the index element
+        /*
+
+         ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Red");
+        arrayList.add("Green");
+        arrayList.add("yellow");
+        arrayList.add("Green");
+        arrayList.add("Purple");
+
+         arrayList.set(2,"Black");
+
+        System.out.println(arrayList);
+
+         */
+
+
+//Get element from an index
+        /*
+         ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Red");
+        arrayList.add("Green");
+        arrayList.add("yellow");
+        arrayList.add("Green");
+        arrayList.add("Purple");
+
+        System.out.println(arrayList.get(2));
+
+         */

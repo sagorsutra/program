695a99abf2e61342d1308cae27b4f31df6ac37cf
Sagor <sagorsutra16@gmail.com>
2022-11-27 20:04:35 +0600

    list element

--- CollectionProblem_W3resource/src/Arraylist/printList.java
+++ CollectionProblem_W3resource/src/Arraylist/printList.java
@@ -1,9 +1,24 @@
 package Arraylist;
 
+import java.util.ArrayList;
+
 public class printList {
+
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Yo pop");
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("Red");
+        arrayList.add("Green");
+        arrayList.add("yellow");
+        arrayList.add("Green");
+        arrayList.add("Purple");
+
+
+        for (String s : arrayList)
+        {
+            System.out.println(s);
+        }
+
     }
 
 }

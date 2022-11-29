4b09439bd3e1298660380404cec3afc70d3759e1
Sagor <sagorsutra16@gmail.com>
2022-11-30 00:00:34 +0600

    contains or not

--- CollectionProblem_W3resource/src/HashMap/Main.java
+++ CollectionProblem_W3resource/src/HashMap/Main.java
@@ -13,12 +13,45 @@ public class Main {
         hashMap.put(4,"Nolan");
         hashMap.put(5,"Halland");
 
-        hashMap.clear();
-
-        for (Map.Entry m: hashMap.entrySet())
-        {
-            System.out.println(m.getKey() + " : " + m.getValue());
+       if (hashMap.containsKey(1)){
+            System.out.println("yes!" + hashMap.get(1) );
         }
+
+
+
 
      }
 }
+
+/* clone a HashMap
+
+    HashMap<Integer,String> hash2 = new HashMap<>(hashMap);
+
+        for (Map.Entry m: hash2.entrySet())
+        {
+            System.out.println(m.getKey() + ": " + m.getValue());
+        }
+
+ */
+
+
+/* init a Hashmap
+
+    HashMap<Integer,String> hashMap = new HashMap<>();
+        hashMap.put(1,"Sagor");
+        hashMap.put(2,"Pollard");
+        hashMap.put(3,"Maria");
+        hashMap.put(4,"Nolan");
+        hashMap.put(5,"Halland");
+
+
+        boolean result = hashMap.isEmpty();
+
+        System.out.println("Map is empty: " + result);
+
+        hashMap.clear();
+        boolean results = hashMap.isEmpty();
+        System.out.println("Map is empty: " + results);
+
+
+ */

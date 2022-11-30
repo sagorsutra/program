57f21a44a268e97fe420e2a560d9ab26c5aa8b45
Sagor <sagorsutra16@gmail.com>
2022-11-30 20:59:41 +0600

    O(n) time complexity for searching subarray which sum is zero By using HashMap

---
+++ CollectionProblem_W3resource/src/HashMap/SubArrwithZero_Sum.java
@@ -0,0 +1,48 @@
+package HashMap;
+
+import javax.swing.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubArrwithZero_Sum {
+
+    public static  boolean haszeroSumSubqury(int[] num)
+    {
+
+
+        Set<Integer> set = new HashSet<>();
+
+        set.add(0);
+        int sum =0;
+
+        for (int value : num)
+        {
+            sum +=value;
+
+            if (set.contains(sum))
+            {
+                return true;
+            }
+
+            set.add(sum);
+
+        }
+
+           return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums = { 4, -6, 3, -1, 4, 2, 7 };
+
+        if (haszeroSumSubqury(nums)) {
+            System.out.println("Subarray exists");
+        }
+        else {
+            System.out.println("Subarray does not exist");
+        }
+
+    }
+
+}

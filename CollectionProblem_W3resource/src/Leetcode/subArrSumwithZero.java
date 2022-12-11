91f937ebc2721eacc37e0f08fc4cff614a62c57b
Sagor <sagorsutra16@gmail.com>
2022-12-11 19:37:27 +0600

    subArrSumwithZero

---
+++ CollectionProblem_W3resource/src/Leetcode/subArrSumwithZero.java
@@ -0,0 +1,39 @@
+package Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class subArrSumwithZero {
+
+  public static  void sumZero(int[] arr)
+        {
+
+            int sum =0;
+            int count = 0;
+            Set<Integer> set = new HashSet<>();
+
+            set.add(0);
+
+            for (int i=0; i< arr.length; i++){
+                sum += arr[i];
+                if (set.contains(sum))
+                {
+                    count++;
+                }
+                set.add(sum);
+            }
+
+            System.out.println(count);
+
+        }
+
+        public static void main(String[] args) {
+
+      int [] arr= {3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
+
+            sumZero(arr);
+
+
+    }
+
+}

915b88bd2c37ec5d5324d6e2708595526ba96383
Sagor <sagorsutra16@gmail.com>
2022-12-18 18:02:54 +0600

    implement operation

--- CollectionProblem_W3resource/src/Linked_List/Node_analysis.java
+++ CollectionProblem_W3resource/src/Linked_List/Node_analysis.java
@@ -20,12 +20,14 @@ public class Node_analysis {
         Node newNode = new Node(data);
         if (head == null)
         {
-            newNode = head;
+            head = newNode;
+            return;
         }
         else
+        {
             newNode.next = head;
             head = newNode;
-
+        }
     }
 
     public void addLast(String data)
@@ -38,12 +40,10 @@ public class Node_analysis {
         else
         {
             Node currnode = head;
-
             while (currnode.next!= null)
             {
                 currnode = currnode.next;
             }
-
             currnode.next = newNode;
         }
     }
@@ -51,12 +51,26 @@ public class Node_analysis {
 
     public void PrintList()
     {
-
+        Node currnode = head;
+        if(head== null)
+        {
+            System.out.println("List is empty");
+            return;
+        }
+        while (currnode!= null)
+        {
+            System.out.print(currnode.data + "->");
+            currnode = currnode.next;
+        }
     }
 
-
     public static void main(String[] args) {
-        Node_analysis ll = new Node_analysis();
+        Node_analysis list = new Node_analysis();
+        list.addFirst("aa");
+        list.addFirst("is ");
+        list.addFirst("Machine");
+        list.PrintList();
+
     }
 
 

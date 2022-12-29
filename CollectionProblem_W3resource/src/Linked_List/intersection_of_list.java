ab0b792320be7dbfa944a6347b582ec55d8f0be0
Sagor <sagorsutra16@gmail.com>
2022-12-29 18:32:32 +0600

    Separating of the values of Intersecting  without hashmap

--- CollectionProblem_W3resource/src/Linked_List/intersection_of_list.java
+++ CollectionProblem_W3resource/src/Linked_List/intersection_of_list.java
@@ -95,7 +95,40 @@ class Solt
             }
             h1=h1.next;
         }
+        Node dummy=new Node(0);
+        Node temp=dummy;
         return nh;
+
+
+        /*
+
+        Node l1=head1;
+        Node l2=head2;
+
+        Node dummy=new Node(0);
+        Node temp=dummy;
+
+        while(l1!=null && l2!=null){
+
+        if(l1.data==l2.data){
+
+            temp.next=new Node(l1.data);
+
+            l1=l1.next;
+            l2=l2.next;
+            temp=temp.next;
+
+        }
+        else if(l1.data<l2.data){
+             l1=l1.next;
+        }
+        else{
+            l2=l2.next;
+          }
+        }
+
+        return dummy.next;
+         */
 
     }
 }

2268bb6875d404e141823f67f22c4a802f002fb1
Sagor <sagorsutra16@gmail.com>
2022-12-29 18:46:11 +0600

    Separating of the values of Intersecting  without hashmap

--- CollectionProblem_W3resource/src/Linked_List/intersection_of_list.java
+++ CollectionProblem_W3resource/src/Linked_List/intersection_of_list.java
@@ -61,7 +61,34 @@ class Solt
 {
     public static Node findIntersection(Node head1, Node head2)
     {
-        // return the head of intersection list
+
+        Node l1 = head1;
+        Node l2 = head2;
+
+        Node dummy = new Node(0);
+        Node temp = dummy;
+
+        while (l1!=null && l2!=null)
+        {
+            if (l1.data == l2.data)
+            {
+                temp.next = new Node(l1.data);
+                l1 = l1.next;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+            else if (l1.data< l2.data) {
+                l1 = l1.next;
+            }
+            else{
+                l2 = l2.next;
+            }
+
+        }
+        return dummy.next;
+
+        /*
+          // return the head of intersection list
         Node nh=null;
         Node nl=nh;
         Node h2=head2;
@@ -98,6 +125,8 @@ class Solt
         Node dummy=new Node(0);
         Node temp=dummy;
         return nh;
+
+         */
 
 
         /*

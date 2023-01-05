c37555b30c1e036301d7d7bb321468af79d881b0
Sagor <sagorsutra16@gmail.com>
2023-01-05 13:41:36 +0600

    Recursion

---
+++ CollectionProblem_W3resource/Recursion/src/Recursion_basic.java
@@ -0,0 +1,5 @@
+public class Recursion_basic {
+    public static void main(String[] args) {
+        System.out.println("Hello this is Recursion");
+    }
+}
---
+++ CollectionProblem_W3resource/Tree/src/treeBasic.java
@@ -0,0 +1,5 @@
+public class treeBasic {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a tree");
+    }
+}

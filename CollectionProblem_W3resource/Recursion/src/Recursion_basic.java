748aef09898c65b139743ed651aa286122ae3a9a
Sagor <sagorsutra16@gmail.com>
2023-01-06 00:33:38 +0600

    Print Sum

--- CollectionProblem_W3resource/Recursion/src/Recursion_basic.java
+++ CollectionProblem_W3resource/Recursion/src/Recursion_basic.java
@@ -1,20 +1,47 @@
 public class Recursion_basic {
 
-    public static void print(int x, int n)
+    public static void  Printsum(int i,int n, int sum)
     {
-        if (x==n+1)
+
+        if (i==n)
         {
+            sum = sum +i;  // Last Sum with last element
+            System.out.println(sum);
             return;
         }
-        System.out.println(x);
-        print(x+1,n);
+
+        sum += i;           // Initial Sum;
+        Printsum(i+1,n,sum);
     }
 
-
     public static void main(String[] args) {
-        int n = 5;
-        print(1,5);
+        Printsum(1,5,0);
+
     }
 
 }
+
+/*
+
+      ---> Number printing
+
+       public static void print(int x, int n)
+          {
+              if (x==n+1)
+              {
+                  return;
+              }
+             System.out.println(x);
+              print(x+1,n);
+          }
+
+
+          public static void main(String[] args) {
+              int n = 5;
+             print(1,5);
+          }
+
+
+
+ */
 

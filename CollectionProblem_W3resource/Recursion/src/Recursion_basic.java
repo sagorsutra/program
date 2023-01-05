8051a238620b2805fa3d674879a11cfca0d3d16d
Sagor <sagorsutra16@gmail.com>
2023-01-06 00:06:26 +0600

    basic number printing

--- CollectionProblem_W3resource/Recursion/src/Recursion_basic.java
+++ CollectionProblem_W3resource/Recursion/src/Recursion_basic.java
@@ -1,5 +1,20 @@
 public class Recursion_basic {
-    public static void main(String[] args) {
-        System.out.println("Hello this is Recursion");
+
+    public static void print(int x, int n)
+    {
+        if (x==n+1)
+        {
+            return;
+        }
+        System.out.println(x);
+        print(x+1,n);
     }
+
+
+    public static void main(String[] args) {
+        int n = 5;
+        print(1,5);
+    }
+
 }
+
